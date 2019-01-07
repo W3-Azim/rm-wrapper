2f762f384bc4e14549bab2369d81e2b80f5f2c52
Azim <azim@w3engineers.com>
2019-01-07 18:44:04 +0600

    first commit

---
+++ RemoveRMApp.cmd
@@ -0,0 +1,3 @@
+rem Uninstall APK, delete .ethereum and delete databases folder for all connected devices
+adb devices | tail -n +2 | cut -sf 1 | xargs -iX adb -s X uninstall io.left.core.rmsample
+adb devices | tail -n +2 | cut -sf 1 | xargs -iX adb -s X shell rm -r sdcard/.rightmesh
